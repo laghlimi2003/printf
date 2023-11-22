c8ff431a5a8f74fb0ebef3cb7b3b7a3368f2ac9c
laghlimi2003 <aghlimieducation.01@gmail.com>
2023-11-22 03:45:28 -0800

    printf

---
+++ handler.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * handler - Format controller
+ * @str: String format
+ * @list: List of arguments
+ *
+ * Return: Total size of arguments with the total size of the base string
+ **/
+int handler(const char *str, va_list list)
+{
+	int size, i, aux;
+
+	size = 0;
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (str[i] == '%')
+		{
+			aux = percent_handler(str, list, &i);
+			if (aux == -1)
+				return (-1);
+
+			size += aux;
+			continue;
+		}
+
+		_putchar(str[i]);
+		size = size + 1;
+	}
+
+
+	return (size);
+}
+
+
